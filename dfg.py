bd795f2a0f2611948273f8ce70aa54c30ef8037c
subina-khader <160305496+subina-khader@users.noreply.github.com>
2024-02-17 12:19:35 +0530

    first commit

---
+++ dfg.py
@@ -0,0 +1,7 @@
+import streamlit as st
+st.title('Air quality index prediction')
+
+st.write("""
+Air quality has always been one of the most important environmental concerns for the general
+public and society. Using machine learning algorithms for Air Quality Index (AQI) prediction is helpful
+for the analysis of fu""")
